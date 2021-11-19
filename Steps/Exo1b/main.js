39852266ff0e33c119c7f2957ffeddcab6e19da3
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-19 21:01:20 +0100

    1-LesBasesDuJavaScriptParLaPratique-LesVariables

---
+++ Steps/Exo1b/main.js
@@ -0,0 +1,11 @@
+var nomDuJoueur1 = "Toto";
+var ageDuJoueur1 = 25;
+var estUnHommeJoueur1 = true;
+
+console.log("Nom du joueur 1 : " + nomDuJoueur1);
+console.log("Age du joueur 1 : " + ageDuJoueur1);
+if(estUnHommeJoueur1 === true) {
+    console.log("Le joueur 1 est un homme");
+} else { // estUnHomme === false
+    console.log("Le joueur 1 est une femme");
+}

65208090b15edb54092f8d6afbe42cbd16a52c19
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-19 21:36:48 +0100

    1-LesBasesDuJavaScriptParLaPratique-3-LesFonctions

--- Tuto/Exo1/main.js
+++ Tuto/Exo1/main.js
@@ -1,29 +1,53 @@
+/**
+ * Variables
+ */
 var nomDuJoueur1 = "Toto";
 var ageDuJoueur1 = 25;
 var estUnHommeJoueur1 = true;
 
 var nomDuJoueur2 = "Tata";
-var ageDuJoueur2 = 28;
+var ageDuJoueur2 = 25;
 var estUnHommeJoueur2 = false;
 
-console.log("---------------------------------------");
+/**
+ * Main
+ */
+afficherLesJoueurs();
+comparerAgeDesDeuxJoueurs();
 
-console.log("Nom du joueur 1 : " + nomDuJoueur1);
-console.log("Age du joueur 1 : " + ageDuJoueur1);
-if(estUnHommeJoueur1 === true) {
-    console.log("Le joueur 1 est un homme");
-} else { // estUnHomme === false
-    console.log("Le joueur 1 est une femme");
+/**
+ * Fonctions
+ */
+function afficherLesJoueurs() {
+    console.log("---------------------------------------");
+
+    console.log("Nom du joueur 1 : " + nomDuJoueur1);
+    console.log("Age du joueur 1 : " + ageDuJoueur1);
+    if(estUnHommeJoueur1 === true) {
+        console.log("Le joueur 1 est un homme");
+    } else { // estUnHomme === false
+        console.log("Le joueur 1 est une femme");
+    }
+
+    console.log("---------------------------------------");
+
+    console.log("%s est le nom du joueur 2", nomDuJoueur2);
+    console.log("%d est l'age du joueur 2", ageDuJoueur2);
+    if(estUnHommeJoueur2 === true) {
+        console.log("Le joueur 2 est un homme");
+    } else { // estUnHomme === false
+        console.log("Le joueur 2 est une femme");
+    }
+
+    console.log("---------------------------------------");
 }
 
-console.log("---------------------------------------");
-
-console.log("%s est le nom du joueur 2", nomDuJoueur2);
-console.log("%d est l'age du joueur 2", ageDuJoueur2);
-if(estUnHommeJoueur2 === true) {
-    console.log("Le joueur 2 est un homme");
-} else { // estUnHomme === false
-    console.log("Le joueur 2 est une femme");
+function comparerAgeDesDeuxJoueurs() {
+    if(ageDuJoueur1 > ageDuJoueur2) { // Le joueur 1 est strictement plus age
+        console.log("Le joueur 1 est le plus age")
+    } else if (ageDuJoueur2 > ageDuJoueur1) { // Le joueur 2 est strictement plus age
+        console.log("Le joueur 2 est le plus age")
+    } else { // valeur égale pour joueur 1 et joueur 2
+        console.log("Les joueurs ont le même age")
+    }
 }
-
-console.log("---------------------------------------");

5fbeb96073ce397353a3e1d5f55f5782f98c23a2
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-20 18:31:42 +0100

    2-UnDebutDeRPGPourApprendreAManipulerLesObjetsEtLesTableaux-7-UneFonctionPourCreerDesEnnemis(Template)

--- Tuto/Exo2/ennemi.js
+++ Tuto/Exo2/ennemi.js
@@ -1,32 +1,47 @@
-var ennemi1 = {
-    nom : "Tortue",
-    description : "Tortue du sud",
-    level : 1,
-    force : 2,
-
-    afficherEnnemi : function() {
-        var texte = "Nom : " + this.nom + "\n";
-        texte += "Description : " + this.description + "\n";
-        texte += "Niveau : " + this.level + "\n";
-        texte += "Force : " + this.force;
-        console.log(texte);
+var templateEnnemi = {    
+    creerTortue : function() {
+        var ennemi1 = {
+            nom : "Tortue",
+            description : "Tortue du sud",
+            level : 1,
+            force : 2,
+        
+            afficherEnnemi : function() {
+                var texte = "Nom : " + this.nom + "\n";
+                texte += "Description : " + this.description + "\n";
+                texte += "Niveau : " + this.level + "\n";
+                texte += "Force : " + this.force;
+                console.log(texte);
+            }
+        }
+        return ennemi1;
+    },  
+    creerCrabe : function() {
+        var ennemi2 = {
+            nom : "Crabe",
+            description : "Crabe du nord",
+            level : 2,
+            force : 4,
+        
+            afficherEnnemi : function() {
+                var texte = "Nom : " + this.nom + "\n";
+                texte += "Description : " + this.description + "\n";
+                texte += "Niveau : " + this.level + "\n";
+                texte += "Force : " + this.force;
+                console.log(texte);
+            }
+        }
+        return ennemi2;
     }
 }
 
-var ennemi2 = {
-    nom : "Crabe",
-    description : "Crabe du nord",
-    level : 2,
-    force : 4,
+var tortue1 = templateEnnemi.creerTortue();
+var tortue2 = templateEnnemi.creerTortue();
+tortue2.level = 2;
+tortue2.force ++;
 
-    afficherEnnemi : function() {
-        var texte = "Nom : " + this.nom + "\n";
-        texte += "Description : " + this.description + "\n";
-        texte += "Niveau : " + this.level + "\n";
-        texte += "Force : " + this.force;
-        console.log(texte);
-    }
-}
+tortue1.afficherEnnemi();
+tortue2.afficherEnnemi();
 
-ennemi1.afficherEnnemi();
-ennemi2.afficherEnnemi();
+var crabe1 = templateEnnemi.creerCrabe();
+crabe1.afficherEnnemi();

d70e8df8a5ed419cbde40b9d6dbb88ac6353b9f6
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-20 22:40:34 +0100

    3-JeuDeQuestion/Reponse-UtiliserLesFichiersJSON-5-ApprofondirLaNotionDeBooleen

--- Tuto/Exo3/main.js
+++ Tuto/Exo3/main.js
@@ -1,9 +1,16 @@
 var questionnaireChat =  require("./questionnaire.json");
 var gestionQuestion =  require("./gestionQuestion");
 
-var question = gestionQuestion.genererQuestionAleatoire(questionnaireChat);
-
-gestionQuestion.afficherUneQuestion(question);
-var reponse = gestionQuestion.saisirReponse();
-var bool = gestionQuestion.estBonneReponse(question,reponse);
-console.log(bool);
+var isGameOver = false;
+while(!isGameOver) {
+    var question = gestionQuestion.genererQuestionAleatoire(questionnaireChat);
+    gestionQuestion.afficherUneQuestion(question);
+    var reponse = gestionQuestion.saisirReponse();
+    var isBonneReponse = gestionQuestion.estBonneReponse(question,reponse);
+    if(isBonneReponse) {
+        console.log("C'est une bonne reponse");
+    } else {
+        console.log("C'est une mauvaise reponse");
+        isGameOver = true;
+    }
+}

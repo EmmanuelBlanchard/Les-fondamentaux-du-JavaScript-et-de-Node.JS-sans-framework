4e2a47f4c34f331cd7fbd245aab493930c585fe0
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-20 21:50:25 +0100

    3-JeuDeQuestion/Reponse-UtiliserLesFichiersJSON-2-RecupererLaTailleDUnObjet

--- Tuto/Exo3/gestionQuestion.js
+++ Tuto/Exo3/gestionQuestion.js
@@ -1,7 +1,25 @@
 var questionnaireChat =  require("./questionnaire.json");
 
-function afficherUneQuestion(question) {
-    console.log(question.desc);
+var questionnaire = {
+    afficherUneQuestion : function(question) {
+        var texte = "";
+        texte += question.desc + "\n";
+        texte += question.reponseA + "\n";
+        texte += question.reponseB + "\n";
+        texte += question.reponseC + "\n";
+        texte += question.reponseD + "\n";
+        console.log(texte);
+    },
+
+    retourneNombreQuestionsDuQuestionnaire : function(questionnaire) {
+        var nombreDeQuestion = 0;
+        for( var question in questionnaire) {
+            nombreDeQuestion++;
+        }
+        return nombreDeQuestion;
+    }
 }
 
-afficherUneQuestion(questionnaireChat.question3);
+questionnaire.afficherUneQuestion(questionnaireChat.question2);
+var nombreDeQuestion = questionnaire.retourneNombreQuestionsDuQuestionnaire(questionnaireChat);
+console.log("Le nombre de questions du questionnaire des chats est de : " + nombreDeQuestion);

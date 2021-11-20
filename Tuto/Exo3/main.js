4ccab4b4df266985c6169d4c439ef1f229e9dfae
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-20 23:03:25 +0100

    3-JeuDeQuestion/Reponse-UtiliserLesFichiersJSON-6-AjouterPlusieursQuestionnaires

--- Tuto/Exo3/main.js
+++ Tuto/Exo3/main.js
@@ -1,9 +1,9 @@
-var questionnaireChat =  require("./questionnaire.json");
+var questionnaire = selectionQuestionnaire();
 var gestionQuestion =  require("./gestionQuestion");
 
 var isGameOver = false;
 while(!isGameOver) {
-    var question = gestionQuestion.genererQuestionAleatoire(questionnaireChat);
+    var question = gestionQuestion.genererQuestionAleatoire(questionnaire);
     gestionQuestion.afficherUneQuestion(question);
     var reponse = gestionQuestion.saisirReponse();
     var isBonneReponse = gestionQuestion.estBonneReponse(question,reponse);
@@ -12,5 +12,14 @@ while(!isGameOver) {
     } else {
         console.log("C'est une mauvaise reponse");
         isGameOver = true;
+    }
+}
+
+function selectionQuestionnaire() {
+    var numeroQuestionnaire = Math.floor(Math.random() * 2 +1);
+    if(numeroQuestionnaire === 1) {
+        return require("./questionnaireChien.json");
+    } else if (numeroQuestionnaire === 2) {
+        return require("./questionnaireChat.json");
     }
 }

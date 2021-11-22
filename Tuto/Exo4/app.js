571c9f7e93e36a96dd15f4686898a001c1950807
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-22 23:15:12 +0100

    4-JavaScript+HTML+CSS:unProgrammeWebCompletSansServeurPourManipulerLeDOM-7-HTML+CSS:LiasonHTMLEtJavaScript

---
+++ Tuto/Exo4/app.js
@@ -0,0 +1,23 @@
+function verifierResultatOrange() {
+    var poids = document.querySelector("#poidsOrange");
+    var poidsValeur = poids.value;
+    var prix = document.querySelector("#prixOrange");
+    var prixValeur = prix.value;
+    console.log(poidsValeur, prixValeur);
+}
+
+function verifierResultatClementine() {
+    var poids = document.querySelector("#poidsClementine");
+    var poidsValeur = poids.value;
+    var prix = document.querySelector("#prixClementine");
+    var prixValeur = prix.value;
+    console.log(poidsValeur, prixValeur);
+}
+
+function verifierResultatFraise() {
+    var poids = document.querySelector("#poidsFraise");
+    var poidsValeur = poids.value;
+    var prix = document.querySelector("#prixFraise");
+    var prixValeur = prix.value;
+    console.log(poidsValeur, prixValeur);
+}

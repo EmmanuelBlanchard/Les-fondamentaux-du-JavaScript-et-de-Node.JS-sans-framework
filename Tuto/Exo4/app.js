ae92687a623d9d27eec02868e3bec96fa0ce4766
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-23 14:14:51 +0100

    4-JavaScript+HTML+CSS:unProgrammeWebCompletSansServeurPourManipulerLeDOM-9-JS:GenerationDuPanier

--- Tuto/Exo4/app.js
+++ Tuto/Exo4/app.js
@@ -1,7 +1,30 @@
+var boiteOutils = require("./boiteOutils");
 var fruitsTemplate = require("./fruits");
 
-var clementine = fruitsTemplate.creerClementine();
-console.log(clementine);
+var panier = genererPanierAleatoire();
+console.log(panier);
+
+function genererPanierAleatoire() {
+    var panier = {
+        oranges : [],
+        clementines : [],
+        fraises : [],
+    }
+    
+    for(var i = 0; i < boiteOutils.genererChiffreAleatoire(2,4); i++) {
+        panier.oranges.push(fruitsTemplate.creerOrange());
+    }
+    
+    for(var i = 0; i < boiteOutils.genererChiffreAleatoire(3,6); i++) {
+        panier.clementines.push(fruitsTemplate.creerClementine());
+    }
+    
+    for(var i = 0; i < boiteOutils.genererChiffreAleatoire(5,10); i++) {
+        panier.fraises.push(fruitsTemplate.creerFraise());
+    }
+    return panier;
+}
+
 
 function verifierResultatOrange() {
     var poids = document.querySelector("#poidsOrange");

01fea389f1152d4cbde502bbdfa875cb13e2f4ca
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-23 19:40:58 +0100

    5-UnPremierServeurWeb!(sansFramework)-4-GererPlusieursPagesHtml

--- Tuto/Exo5/serveur.js
+++ Tuto/Exo5/serveur.js
@@ -1,13 +1,21 @@
 var http = require("http");
 var fs = require("fs");
-
+var url = require("url");
 const PORT = "8080";
 
 var serveur = http.createServer(traiteReq);
 serveur.listen(PORT);
 
 function traiteReq(requete, reponse) {
-    var pageHtml = fs.readFileSync("./index.html","UTF-8");
+    var pageHtml = "";
+    var monObjUrl = url.parse(requete.url);
+    
+    if(monObjUrl.pathname === "/index.html" || monObjUrl.pathname === "/") {
+        pageHtml = fs.readFileSync("./index.html","UTF-8");
+    } else if(monObjUrl.pathname === "/toto.html") {
+        pageHtml = fs.readFileSync("./toto.html","UTF-8");
+    } 
+
     reponse.writeHead(200,{'Content-Type' : "text/html"});
     reponse.write(pageHtml);
     reponse.end();

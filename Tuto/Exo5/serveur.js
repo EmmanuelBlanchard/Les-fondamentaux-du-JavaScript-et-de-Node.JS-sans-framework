b3f48abb9ed672fd566f830874a174e81151cd56
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-23 19:00:57 +0100

    5-UnPremierServeurWeb!(sansFramework)-2-EnvoyerUneReponseAuClient

--- Tuto/Exo5/serveur.js
+++ Tuto/Exo5/serveur.js
@@ -5,5 +5,8 @@ var serveur = http.createServer(traiteReq);
 serveur.listen(PORT);
 
 function traiteReq(requete, reponse) {
-    console.log("coucou");
+    var pageHtml = '<!DOCTYPE html><head></head><body><h1>coucou</h1></body></html>';
+    reponse.writeHead(200,{'Content-Type' : "text/html"});
+    reponse.write(pageHtml);
+    reponse.end();
 }

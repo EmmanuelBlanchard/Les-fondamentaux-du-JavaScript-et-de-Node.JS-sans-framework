b0aef724c30a819cf70d36bc287d747704593e43
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-23 20:30:22 +0100

    5-UnPremierServeurWeb!(sansFramework)-5-renvoyerDuCssEtDesImages

--- Tuto/Exo5/serveur.js
+++ Tuto/Exo5/serveur.js
@@ -8,15 +8,26 @@ serveur.listen(PORT);
 
 function traiteReq(requete, reponse) {
     var pageHtml = "";
+    var contentType = "";
     var monObjUrl = url.parse(requete.url);
     
+    console.log(monObjUrl.pathname);
+
     if(monObjUrl.pathname === "/index.html" || monObjUrl.pathname === "/") {
         pageHtml = fs.readFileSync("./index.html","UTF-8");
+        contentType = "text/html";
     } else if(monObjUrl.pathname === "/toto.html") {
         pageHtml = fs.readFileSync("./toto.html","UTF-8");
-    } 
+        contentType = "text/html";
+    } else if(monObjUrl.pathname === "/css.css") {
+        pageHtml = fs.readFileSync("./css.css");
+        contentType = "text/css";
+    } else if(monObjUrl.pathname === "/panier.jpg") {
+        pageHtml = fs.readFileSync("./images/panier.jpg");
+        contentType = "image/jpg";
+    }
 
-    reponse.writeHead(200,{'Content-Type' : "text/html"});
+    reponse.writeHead(200,{'Content-Type' : contentType});
     reponse.write(pageHtml);
     reponse.end();
 }

786e30226acc0036e08652120d5fee828a441049
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-23 19:14:10 +0100

    5-UnPremierServeurWeb!(sansFramework)-3-EnvoyerUnePageHtmlEtInstallationDeNodemon

--- Tuto/Exo5/serveur.js
+++ Tuto/Exo5/serveur.js
@@ -1,11 +1,13 @@
 var http = require("http");
+var fs = require("fs");
+
 const PORT = "8080";
 
 var serveur = http.createServer(traiteReq);
 serveur.listen(PORT);
 
 function traiteReq(requete, reponse) {
-    var pageHtml = '<!DOCTYPE html><head></head><body><h1>coucou</h1></body></html>';
+    var pageHtml = fs.readFileSync("./index.html","UTF-8");
     reponse.writeHead(200,{'Content-Type' : "text/html"});
     reponse.write(pageHtml);
     reponse.end();

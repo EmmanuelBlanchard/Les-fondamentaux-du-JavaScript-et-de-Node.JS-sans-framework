bcad30874cf9b29e867edc9dd58e593422738041
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-23 23:14:12 +0100

    5-UnPremierServeurWeb!(sansFramework)-9-RestructuractionDuServeur

--- Tuto/Exo5/serveur.js
+++ Tuto/Exo5/serveur.js
@@ -1,7 +1,7 @@
 var http = require("http");
-var fs = require("fs");
 var url = require("url");
 var queryString = require("querystring");
+var gestionPage = require("./gestionPage");
 require("remedial");
 const PORT = "8080";
 
@@ -9,51 +9,13 @@ var serveur = http.createServer(traiteReq);
 serveur.listen(PORT);
 
 function traiteReq(requete, reponse) {
-    var dataAEnvoyer = "";
-    var contentType = "";
-    var dossier = "";
     var monObjUrl = url.parse(requete.url);
     var monObjQuery = queryString.parse(monObjUrl.query);
 
     if(monObjUrl.pathname === "/") {
         monObjUrl.pathname = "/index.html";
     }
-    var indexDuPoint = monObjUrl.pathname.indexOf(".");
-    var extension = monObjUrl.pathname.substring(indexDuPoint, monObjUrl.pathname.length);
-
-    var encodage = "";
-
-    switch(extension) {
-        case ".html" : 
-            contentType = "text/html";
-            dossier = "html/";
-            encodage = "UTF-8";
-        break;
-        case ".css" : 
-            contentType = "text/css";
-            dossier = "css/";
-        break;
-        case ".jpg" : 
-            contentType = "image/jpeg";
-            dossier = "images/";
-        break;
-        default : contentType = "";
-        break;
-    }
-
-    if(monObjUrl.pathname !== "/favicon.ico") {
-        try {
-            dataAEnvoyer = fs.readFileSync(dossier+monObjUrl.pathname.substring(1,monObjUrl.pathname.length),encodage);
-        } catch (error) {
-            console.log("ici"+error);
-        }
-        
-        if(extension === ".html") {
-            dataAEnvoyer = dataAEnvoyer.supplant(monObjQuery);
-        }
-    }
-
-    reponse.writeHead(200,{'Content-Type' : contentType});
-    reponse.write(dataAEnvoyer);
-    reponse.end();
+    
+    var data = gestionPage.preparerLesDonnees(monObjUrl, monObjQuery);
+    gestionPage.envoyerLesDonnees(data, reponse);
 }

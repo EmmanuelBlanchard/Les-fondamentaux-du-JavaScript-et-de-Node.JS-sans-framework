15f1efcbe2a1693f24e7389f8be46df952e0c71d
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-23 22:01:55 +0100

    5-UnPremierServeurWeb!(sansFramework)-7-QueryStringEtModuleRemedial

--- Tuto/Exo5/serveur.js
+++ Tuto/Exo5/serveur.js
@@ -1,6 +1,8 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
+var queryString = require("querystring");
+require("remedial");
 const PORT = "8080";
 
 var serveur = http.createServer(traiteReq);
@@ -11,14 +13,18 @@ function traiteReq(requete, reponse) {
     var contentType = "";
     var dossier = "";
     var monObjUrl = url.parse(requete.url);
+    var monObjQuery = queryString.parse(monObjUrl.query);
+    console.log(monObjQuery);
     var indexDuPoint = monObjUrl.pathname.indexOf(".");
     var extension = monObjUrl.pathname.substring(indexDuPoint, monObjUrl.pathname.length);
 
-    
+    var encodage = "";
+
     switch(extension) {
         case ".html" : 
             contentType = "text/html";
             dossier = "html/";
+            encodage = "UTF-8";
         break;
         case ".css" : 
             contentType = "text/css";
@@ -33,7 +39,10 @@ function traiteReq(requete, reponse) {
     }
 
     if(monObjUrl.pathname !== "/favicon.ico") {
-        dataAEnvoyer = fs.readFileSync(dossier+monObjUrl.pathname.substring(1,monObjUrl.pathname.length));
+        dataAEnvoyer = fs.readFileSync(dossier+monObjUrl.pathname.substring(1,monObjUrl.pathname.length),encodage);
+        if(monObjUrl.pathname === "/toto.html") {
+            dataAEnvoyer = dataAEnvoyer.supplant(monObjQuery);
+        }
     }
 
     reponse.writeHead(200,{'Content-Type' : contentType});
